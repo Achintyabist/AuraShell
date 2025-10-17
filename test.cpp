4f07f44db9f1cc820a28afb4b00baa6f24f50be8
Achintya Bist <achintyabist05@gmail.com>
2025-10-17 14:55:45 +0530

    code

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 #include<string>
 #include<sstream>
 #include<vector>
-#include<cstdlib>      
-#include<unistd.h> 
+#include<cstdlib>
+#include<unistd.h>
 
 using namespace std;
 unordered_map<string, function<void(string)>> commands;
@@ -48,14 +48,13 @@ void typeOfCommand(string command){
         auto it = commands.find(str[1]);
         if(it != commands.end())
         {
-            if(it->first == "exit" || it->first == "echo" || it->first == "type"){
             cout<<str[1]<<" is a shell builtin"<<endl;
             exist=true;
-            }
         }
     }    
-        char* path=getenv("PATH");
-        
+        if(!exist)
+        {
+            char* path=getenv("PATH");
             if(path!=NULL)
             {
                 string spath(path);
@@ -68,8 +67,8 @@ void typeOfCommand(string command){
                         {
                             string fullpath= directory +"/"+str[1];
                             if(access(fullpath.c_str(),X_OK) == 0){
-                            cout<<fullpath<<endl;
-                            exist=true;
+                                cout<<fullpath<<endl;
+                                exist=true;
                             }
                         }
                         directory.clear();
@@ -80,17 +79,85 @@ void typeOfCommand(string command){
                     }
                 }
             }
+
             if(!exist)
-            {
                cout<< str[1] <<": command not found\n";
-            }
+        }
 }
+
+void cd(string command)
+{
+    stringstream ss(command);
+    vector<string> str;
+    string word;
+    while(ss >> word){
+        str.push_back(word);
+    }
+    str.erase(str.begin());
+    if(str.size()<1)
+    {
+        return;
+    }
+    if(str[0]=="~")
+    {
+        char*path=getenv("HOME");
+        str[0]=path;
+    }
+    if(chdir(str[0].c_str())!=0)
+    {
+        cout<<"cd: "<<str[0]<<": No such file or directory"<<endl;
+    }
+}
+
+void pwd(string command) {
+    char cwd[1000];
+    getcwd(cwd,sizeof(cwd));
+    cout << cwd << endl;
+}
+
+void run_exec(string command){
+    pid_t pid = fork();
+    stringstream ss(command);
+    vector<string> str;
+    string word;
+
+    while(ss >> word){
+        str.push_back(word);
+    }
+
+    int n = str.size();
+    char *args[n+1];
+
+    for(int i=0;i<n;i++){
+        args[i] = (char *)str[i].c_str();
+    }
+    args[n] = NULL;
+
+    if(pid>0){
+        wait(NULL);
+    }
+    else if(pid==0){
+        if(args[0][0]=='.' && args[0][1]=='/'){
+            execv(args[0], args);
+            perror("execv");
+        }
+        else{
+            int is_err = execvp(args[0], args);
+            if(is_err == -1){
+                cout<< str[0] <<": command not found\n";
+            }
+        }
+    }
+}
+
 int main(){
     string command;
     commands ={
         {"exit", exitShell},
         {"echo", echo},
-        {"type", typeOfCommand}
+        {"type", typeOfCommand},
+        {"cd", cd},
+        {"pwd", pwd},
     };
 
     
@@ -112,8 +179,9 @@ int main(){
         if(it != commands.end()){
             it->second(command);
         }
-        else{
-            cout<< str <<": command not found\n";
+        else {
+            run_exec(command);
+            cout<<endl;
         }
     }
 }
